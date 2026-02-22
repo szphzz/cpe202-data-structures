7cbf3e0aa509116c166aac796ff9c0f90daa00cd
Sophia Chung <55817816+szphzz@users.noreply.github.com>
2026-02-22 16:00:42 +0100

    Delete cpe202 directory

--- cpe202/lab0/planets.py
+++
@@ -1,8 +0,0 @@
-def weight_on_planets():
-   earth = float(input('What do you weigh on earth? '))
-   mars = earth * 0.38
-   jupiter = earth * 2.34
-   print('\nOn Mars you would weigh', mars, 'pounds.\nOn Jupiter you would weigh', jupiter, 'pounds.')
-   
-if __name__ == '__main__':
-   weight_on_planets()
